712d44382f8263c1e3364d36b24189fc613d1497
Taylor Cox <taylorcox.dev@gmail.com>
2018-03-27 15:24:26 -0400

    v1.2.0 (again)

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ export const store = new Vuex.Store({
 	state: {
 		config: {
 			appTitle: 'New York Times',
-			appVersion: 'v1.1.2',
+			appVersion: 'v1.2.0',
 			latestUrl: 'https://api.github.com/repos/alteryx-vue/nyt-connector/releases/latest',
 			updateAvail: false,
 			updatePrompt: false,
